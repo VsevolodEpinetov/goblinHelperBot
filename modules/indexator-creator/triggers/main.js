2278d2a2b65c34bb82a92bc3f31e929a1b314e0d
Vsevolod Epinetov <vsevolod@epinetov.com>
2023-11-17 18:59:06 +0300

    new: if studios' amount is more than threshold value

--- modules/indexator-creator/triggers/main.js
+++ modules/indexator-creator/triggers/main.js
@@ -26,7 +26,7 @@ module.exports = Composer.on('channel_post', async (ctx) => {
         ctx.replyWithHTML('Бип буп, записал канал. Присылай индексаторы \n\nПришли любое сообщение, которое будет содержать хотя бы 1 эмодзи "🔸" - я запомню его как Индексатор. \n\n<i>Рекомендую прислать минимум <b>2</b> таких сообщения</i>')
       }
       sessionInstance.saveSession('channelsSession', localChannels);
-      
+
     } else
 
       if (messageText === 'stop') {
@@ -35,136 +35,167 @@ module.exports = Composer.on('channel_post', async (ctx) => {
         localChannels.channels[channelID].locked = true;
         sessionInstance.saveSession('channelsSession', localChannels);
 
-      } else 
-      
-
-      if (messageText === 'start') {
-
-        ctx.replyWithHTML('🤖 Индексаторы в автоматическом режиме')
-        localChannels.channels[channelID].locked = false;
-        sessionInstance.saveSession('channelsSession', localChannels);
-
-      } else 
-      
-      
-      if (messageText === 'switch') {
-
-        if (localChannels.channels[channelID].type === 'collection') {
-          localChannels.channels[channelID].type = 'archive';
-          ctx.replyWithHTML('Режим - архив БГ')
-        } else if (localChannels.channels[channelID].type === 'archive') {
-          localChannels.channels[channelID].type = 'collection';
-          ctx.replyWithHTML('Режим - коллекция')
-        }
-        sessionInstance.saveSession('channelsSession', localChannels);
-
-      } else 
+      } else
 
 
-      if (messageText === 'reset') {
+        if (messageText === 'start') {
 
-        localChannels.channels[channelID] = {
-          indexers: [],
-          studios: [],
-          locked: false,
-          type: 'archive'
-        };
+          ctx.replyWithHTML('🤖 Индексаторы в автоматическом режиме')
+          localChannels.channels[channelID].locked = false;
+          sessionInstance.saveSession('channelsSession', localChannels);
 
-        ctx.replyWithHTML('Стёр всю информацию по каналу, можешь удалять старые индексаторы и присылать новые. \n\nПришли любое сообщение, которое будет содержать хотя бы 1 эмодзи "🔸" - я запомню его как Индексатор. \n\n<i>Рекомендую прислать минимум <b>2</b> таких сообщения</i>')
-        sessionInstance.saveSession('channelsSession', localChannels);
+        } else
 
-      } else 
-      
-      
-      if (!localChannels.channels[channelID].locked) {
-        if (!localChannels.channels[channelID].type) localChannels.channels[channelID].type = 'archive';
-        //console.log(ctx.channelPost)
-        if (messageText.indexOf('🔸') < 0) {
-          if (localChannels.channels[channelID].indexers.length === 0) {
-            ctx.replyWithHTML('Нет ни одного записанного индексатора! \n\nПришли любое сообщение, которое будет содержать хотя бы 1 эмодзи "🔸" - я запомню его как Индексатор. \n\n<i>Рекомендую прислать минимум <b>2</b> таких сообщения</i>');
-          } else {
-            let studioName = '';
-            let monthName = messageText.split('\n')[0]?.split(' (')[1]?.split(' ')[0] || 'пыпы';
-            let year = messageText.split('\n')[0]?.split(' (')[1]?.split(' ')[1]?.split(')')[0] || '2222';
-            const months = {
-              'январь': '01',
-              'февраль': '02',
-              'март': '03',
-              'апрель': '04',
-              'май': '05',
-              'июнь': '06',
-              'июль': '07',
-              'август': '08',
-              'сентябрь': '09',
-              'октябрь': '10',
-              'ноябрь': '11',
-              'декабрь': '12',
-              'пыпы': '88'
+
+          if (messageText === 'switch') {
+
+            if (localChannels.channels[channelID].type === 'collection') {
+              localChannels.channels[channelID].type = 'archive';
+              ctx.replyWithHTML('Режим - архив БГ')
+            } else if (localChannels.channels[channelID].type === 'archive') {
+              localChannels.channels[channelID].type = 'collection';
+              ctx.replyWithHTML('Режим - коллекция')
             }
-            let releaseName = '';
-            if (messageText.indexOf('\n') > 0) {
-              studioName = messageText.split('\n')[0].split(' (')[0];
-              if (year == '2222' || months[monthName] == '88') {
-                releaseName = `${messageText.split('\n')[1]}`;
-              } else {
-                releaseName = `${year}${months[monthName]} - ${messageText.split('\n')[1]}`;
+            sessionInstance.saveSession('channelsSession', localChannels);
+
+          } else
+
+
+            if (messageText === 'reset') {
+
+              localChannels.channels[channelID] = {
+                indexers: [],
+                studios: [],
+                locked: false,
+                type: 'archive'
+              };
+
+              ctx.replyWithHTML('Стёр всю информацию по каналу, можешь удалять старые индексаторы и присылать новые. \n\nПришли любое сообщение, которое будет содержать хотя бы 1 эмодзи "🔸" - я запомню его как Индексатор. \n\n<i>Рекомендую прислать минимум <b>2</b> таких сообщения</i>')
+              sessionInstance.saveSession('channelsSession', localChannels);
+
+            } else
+
+
+              if (!localChannels.channels[channelID].locked) {
+                if (!localChannels.channels[channelID].type) localChannels.channels[channelID].type = 'archive';
+                //console.log(ctx.channelPost)
+                if (messageText.indexOf('🔸') < 0) {
+                  if (localChannels.channels[channelID].indexers.length === 0) {
+                    ctx.replyWithHTML('Нет ни одного записанного индексатора! \n\nПришли любое сообщение, которое будет содержать хотя бы 1 эмодзи "🔸" - я запомню его как Индексатор. \n\n<i>Рекомендую прислать минимум <b>2</b> таких сообщения</i>');
+                  } else {
+                    let studioName = '';
+                    let monthName = messageText.split('\n')[0]?.split(' (')[1]?.split(' ')[0] || 'пыпы';
+                    let year = messageText.split('\n')[0]?.split(' (')[1]?.split(' ')[1]?.split(')')[0] || '2222';
+                    const months = {
+                      'январь': '01',
+                      'февраль': '02',
+                      'март': '03',
+                      'апрель': '04',
+                      'май': '05',
+                      'июнь': '06',
+                      'июль': '07',
+                      'август': '08',
+                      'сентябрь': '09',
+                      'октябрь': '10',
+                      'ноябрь': '11',
+                      'декабрь': '12',
+                      'пыпы': '88'
+                    }
+                    let releaseName = '';
+                    if (messageText.indexOf('\n') > 0) {
+                      studioName = messageText.split('\n')[0].split(' (')[0];
+                      if (year == '2222' || months[monthName] == '88') {
+                        releaseName = `${messageText.split('\n')[1]}`;
+                      } else {
+                        releaseName = `${year}${months[monthName]} - ${messageText.split('\n')[1]}`;
+                      }
+                    } else {
+                      studioName = messageText.split(' (')[0];
+                      releaseName = `${year}${months[monthName]}`;
+                    }
+
+                    let copy = localChannels.channels[channelID].studios;
+                    const newPostInfo = {
+                      name: studioName,
+                      release: releaseName,
+                      messageID: ctx.channelPost.message_id
+                    }
+                    copy.push(newPostInfo)
+                    copy.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
+                    let newTextFirst = ``;
+                    let newTextSecond = ``;
+                    if (copy.length < 100) {
+                      newTextFirst = `🔸 <b>Индексатор 1</b>🔸\n\n`
+                      copy.forEach(st => {
+                        if (localChannels.channels[channelID].type === 'archive')
+                          newTextFirst += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.name} - ${st.release.split(' - ')[1] || st.release}</a>\n`
+                        if (localChannels.channels[channelID].type === 'collection')
+                          newTextFirst += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.release}</a>\n`
+                      });
+                    } else {
+                      newTextFirst = `🔸 <b>Индексатор 1</b>🔸\n\n`
+                      newTextSecond = `🔸 <b>Индексатор 2</b>🔸\n\n`
+                      let counter = 1;
+                      copy.forEach(st => {
+                        if (localChannels.channels[channelID].type === 'archive') {
+                          if (counter > 79) {
+                            newTextSecond += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.name} - ${st.release.split(' - ')[1] || st.release}</a>\n`
+                          } else {
+                            newTextFirst += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.name} - ${st.release.split(' - ')[1] || st.release}</a>\n`
+                          }
+                        }
+                        if (localChannels.channels[channelID].type === 'collection')
+                          if (counter > 79) {
+                            newTextSecond += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.release}</a>\n`
+                          } else {
+                            newTextFirst += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.release}</a>\n`
+                          }
+                      });
+                    }
+
+                    try {
+                      ctx.telegram.editMessageText(channelID, localChannels.channels[channelID].indexers[0].messageID, undefined, newTextFirst, {
+                        parse_mode: "HTML"
+                      }).catch(e => {
+                        console.log(e)
+                      })
+                    } catch (e) {
+                      console.log(e)
+                    }
+
+                    if (newTextSecond.length > 5) {
+                      try {
+                        ctx.telegram.editMessageText(channelID, localChannels.channels[channelID].indexers[1].messageID, undefined, newTextSecond, {
+                          parse_mode: "HTML"
+                        }).catch(e => {
+                          console.log(e)
+                        })
+                      } catch (e) {
+                        console.log(e)
+                      }
+                    }
+                  }
+                } else {
+
+                  const numberOfIndexer = localChannels.channels[channelID].indexers.length + 1;
+                  const defaultText = `🔸 <b>Индексатор ${numberOfIndexer}</b>🔸\n\n<i>Скоро тут будут ссылки на релизы!</i>`;
+                  localChannels.channels[channelID].indexers.push({
+                    messageID: ctx.channelPost.message_id
+                  })
+                  try {
+                    ctx.telegram.editMessageText(channelID, ctx.channelPost.message_id, undefined, defaultText, {
+                      parse_mode: "HTML"
+                    }).catch(e => {
+                      console.log(e)
+                    })
+                  } catch (e) {
+                    console.log(e)
+                  }
+                  await ctx.telegram.pinChatMessage(channelID, ctx.channelPost.message_id);
+                }
+
+                sessionInstance.saveSession('channelsSession', localChannels);
               }
-            } else {
-              studioName = messageText.split(' (')[0];
-              releaseName = `${year}${months[monthName]}`;
-            }
-
-            let copy = localChannels.channels[channelID].studios;
-            const newPostInfo = {
-              name: studioName,
-              release: releaseName,
-              messageID: ctx.channelPost.message_id
-            }
-            copy.push(newPostInfo)
-            copy.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
-
-            let newText = ``;
-            if (copy.length < 100) {
-              newText = `🔸 <b>Индексатор 1</b>🔸\n\n`
-              copy.forEach(st => {
-                if (localChannels.channels[channelID].type === 'archive') 
-                  newText += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.name} - ${st.release.split(' - ')[1] || st.release}</a>\n`
-                if (localChannels.channels[channelID].type === 'collection') 
-                  newText += `<a href="https://t.me/c/${channelID.toString().split('-100')[1]}/${st.messageID}">${st.release}</a>\n`
-              });
-            }
-
-            try {
-              ctx.telegram.editMessageText(channelID, localChannels.channels[channelID].indexers[0].messageID, undefined, newText, {
-                parse_mode: "HTML"
-              }).catch(e => {
-                console.log(e)
-              })
-            } catch (e) {
-              console.log(e)
-            }
-
-          }
-        } else {
-
-          const numberOfIndexer = localChannels.channels[channelID].indexers.length + 1;
-          const defaultText = `🔸 <b>Индексатор ${numberOfIndexer}</b>🔸\n\n<i>Скоро тут будут ссылки на релизы!</i>`;
-          localChannels.channels[channelID].indexers.push({
-            messageID: ctx.channelPost.message_id
-          })
-          try {
-            ctx.telegram.editMessageText(channelID, ctx.channelPost.message_id, undefined, defaultText, {
-              parse_mode: "HTML"
-            }).catch(e => {
-              console.log(e)
-            })
-          } catch (e) {
-            console.log(e)
-          }
-          await ctx.telegram.pinChatMessage(channelID, ctx.channelPost.message_id);
-        }
-
-        sessionInstance.saveSession('channelsSession', localChannels);
-      }
   }
 })
