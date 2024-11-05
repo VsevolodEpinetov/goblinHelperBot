789c4a277feacde31a8420cba404096d44547bd1
Vsevolod Epinetov <vsevolod@epinetov.com>
2024-11-05 04:13:17 +0300

    fix: relations

--- modules/polls/index.js
+++ modules/polls/index.js
@@ -2,12 +2,6 @@ const { Composer } = require('telegraf')
 
 module.exports = Composer.compose([
   require('./commands/count'),
-  require('./commands/poll'),
-  require('./commands/studios'),
-  require('./commands/sync'),
-  require('./commands/switch'),
   require('./commands/add'),
   require('./commands/addByPlus'),
-  require('./commands/remove'),
-  //require('./reactions/add')
 ])
