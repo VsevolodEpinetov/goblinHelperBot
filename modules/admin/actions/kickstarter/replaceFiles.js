2f1e5a5067eecb8de144bf86d0d602d1a939415d
Vsevolod Epinetov <vsevolod@epinetov.com>
2024-12-27 15:16:51 +0300

    new: replacing ks files

---
+++ modules/admin/actions/kickstarter/replaceFiles.js
@@ -0,0 +1,16 @@
+const { Composer, Markup } = require("telegraf");
+const util = require('../../../util');
+const SETTINGS = require('../../../../settings.json');
+
+module.exports = Composer.action(/^replaceFilesKickstarter_/g, async (ctx) => {
+  try {
+    await ctx.deleteMessage(ctx.callbackQuery.message.message_id);
+  } catch (e) {
+    await ctx.replyWithHTML(`Из-за ограничений телеграма тебе нужно использовать /start ещё раз. Старое сообщение останется, можешь его удалить вручную, если мешает.`)
+    return;
+  }
+  const ksId = ctx.callbackQuery.data.split('_')[1];
+  ctx.session.editingKickstarter = {};
+  ctx.session.editingKickstarter.id = ksId;
+  ctx.scene.enter('ADMIN_SCENE_REPLACE_KICKSTARTER_FILES');
+});
---
+++ modules/admin/scenes/kickstarters/replaceFiles.js
@@ -0,0 +1,53 @@
+const { Scenes, Markup } = require("telegraf");
+
+const currentStageName = 'ADMIN_SCENE_REPLACE_KICKSTARTER_FILES'
+
+const adminReplaceKickstarterFiles = new Scenes.BaseScene(currentStageName);
+
+adminReplaceKickstarterFiles.enter(async (ctx) => {
+  await ctx.telegram.editMessageText(ctx.session.chatID, ctx.session.toEdit, undefined, `Пришли <b>файлы</b> проекта`, {
+    parse_mode: "HTML"
+  });
+});
+
+adminReplaceKickstarterFiles.on('document', async (ctx) => {
+  ctx.session.editingKickstarter.files = [];
+  ctx.session.editingKickstarter.files.push(ctx.message.document.file_id);
+
+  await ctx.deleteMessage(ctx.message.message_id);
+
+  await ctx.telegram.editMessageText(ctx.session.chatID, ctx.session.toEdit, undefined, `Пришли <b>файлы</b> проекта\nКоличество файлов: ${ctx.session.editingKickstarter.files.length}`, {
+    parse_mode: "HTML",
+    ...Markup.inlineKeyboard([
+      Markup.button.callback('Готово', 'finished')
+    ])
+  });
+});
+
+adminReplaceKickstarterFiles.action('finished', async (ctx) => {
+  ctx.scene.leave();
+});
+
+adminReplaceKickstarterFiles.leave(async (ctx) => {
+  if (!ctx.kickstarters.list) ctx.kickstarters.list = [];
+  const kstID = ctx.editingKickstarter.id;
+  ctx.kickstarters.list[kstID].files = ctx.sesssion.editingKickstarter.files;
+
+  ctx.telegram.editMessageText(ctx.session.chatID, ctx.session.toEdit, undefined, `♻️ Кикстартер успешно отредактрован с ID ${kstID}.\nКоличество файлов: ${ctx.kickstarters.list[kstID].files.length}`, {
+    parse_mode: "HTML",
+    ...Markup.inlineKeyboard([
+      [
+        Markup.button.callback('+', 'adminAddKickstarter'),
+        Markup.button.callback('🔍', 'searchKickstarter'),
+        Markup.button.callback('✏️', 'adminEditKickstarter')
+      ],
+      [
+        Markup.button.callback('←', 'adminMenu')
+      ]
+    ])
+  })
+
+  ctx.sesssion.editingKickstarter = null;
+});
+
+module.exports = adminAddKickstarterFiles;
